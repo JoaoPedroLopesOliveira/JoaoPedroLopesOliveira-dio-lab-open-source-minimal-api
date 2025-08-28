67645cccf88c23ac7f6268abc82136e8bd945a6a
João Oliveira <joao.ol.dev@gmail.com>
2025-08-28 18:18:52 -0300

    cria estrutura base do projeto

---
+++ Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+
+app.MapGet("/", () => "teste hot Reload");
+
+app.MapPost("/login", (minimal_api.Dominio.DTOs.LoginDTO loginDTO) =>
+{
+    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+    {
+        return Results.Ok("Logado com sucesso");
+    }
+    else
+    {
+        return Results.Unauthorized();
+    }
+});
+
+
+app.Run();
+
